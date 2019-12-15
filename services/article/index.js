322f29f4d586fd3d3172b7ca688d928427be0e3e
muitsfriday <mu.itsfriday@gmail.com>
2019-12-15 13:27:58 +0700

    refactor article service

--- services/article/index.js
+++ services/article/index.js
@@ -1,141 +1,28 @@
 const express = require('express')
 const mongo = require('./db/mongo')
 const bodyParser = require('body-parser')
-const jwt = require('jsonwebtoken')
-const jwtVerify = require('express-jwt')
+
+const mainRouter = require('./router/main').default
+const adminRouter = require('./router/admin').default
 
 const url = process.env.MONGO_URL
 const dbName = process.env.DB_NAME
-const jwtSecret = process.env.JWT_SECRET
-
-const auth = jwtVerify({ secret: jwtSecret })
 
 let db = null
-mongo.connect(url, dbName, (instance) => {
+mongo.connect(url, dbName, instance => {
 	db = instance
+	router(db)
 })
 
-const app = express()
+function router(db) {
+  const app = express()
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+	app.use('/', mainRouter(db))
+	app.use('/', adminRouter(db))
 
-app.get('/', (req, res) => {
-	console.log('req.xxx: ', req.xxx)
-	const collecton = db.collection('article')
-	const getArticlesTask = collecton.find({
-		deleteAt: { $exists: false }
-	}, {
-		projection: { content: 0 }
-	}).toArray()
-
-	getArticlesTask
-		.then((result) => {
-			res.send({ list: result })
-		})
-		.catch((err) => {
-			res.send({ list: [], err: err })
-		})
-})
-
-
-app.get('/:id', (req, res) => {
-	const id = mongo.toID(req.params.id)
-	const collecton = db.collection('article')
-
-	collecton.findOne({ 
-		_id: id, 
-		deleteAt: { $exists: false } 
-	})
-	.then(result => {
-		res.status(result ? 200 : 404).send({ data: result })
-	})
-	.catch(err => {
-		res.status(500).send({ data: null, err: err })
-	})
-
-})
-
-
-app.post('/', auth, (req, res) => {
-	const title = req.body.title
-	const content = req.body.content
-	const username = req.user.username
-
-	const collecton = db.collection('article')
-	collecton.insertOne({
-		title,
-		content,
-		username,
-		createAt: new Date(),
-		updateAt: new Date(),
-	}).then(result => {
-		res.send({ status: true })
-	}).catch(err => {
-		res.send({ status: false, err })
-	})
-})
-
-
-app.post('/:id', auth, (req, res) => {
-	const id = mongo.toID(req.params.id)
-	const title = req.body.title || false
-	const content = req.body.content || false
-	const username = req.user.username
-
-	const updates = {}
-	if (title !== false) {
-		updates.title = title
-	}
-	if (content !== false) {
-		updates.content = content
-	}
-
-	const collecton = db.collection('article')
-	collecton.updateOne({ _id: id, username }, { $set: updates })
-	.then(result => {
-		res.send({ status: result.result.ok > 0 })
-	})
-	.catch(err => {
-		res.send({ status: false, err })
-	})
-})
-
-
-app.delete('/:id', auth, (req, res) => {
-	const id = mongo.toID(req.params.id)
-	const username = req.user.username
-	const collecton = db.collection('article')
-
-	console.log('(1)', id)
-
-	collecton.findOne({ _id: id })
-	.then(result => {
-		console.log('(2)', result)
-		if (!result) {
-			return res.send({ status: false, message: 'article not found' })
-		}
-
-		if (result.username !== username) {
-			return res.status(401).send({ status: false, message: 'not authorized' })
-		}
-
-		return collecton.updateOne(
-			{ _id: id }, 
-			{ $set: { deleteAt: new Date() } 
-		})
-	})
-	.then(result => {
-		res.send({ status: result.result.ok > 0 })
-	})
-	.catch(err => {
-		res.status(500).send({ status: false, err })
-	})
-
-})
-
-
-
-app.listen(process.env.PORT, () => {
-	console.log('service user listening at port: ' + process.env.PORT)
-})
+  app.listen(process.env.PORT, () => {
+    console.log('service user listening at port: ' + process.env.PORT)
+  })
+}
---
+++ services/web-content/store.js
@@ -0,0 +1,88 @@
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import axios from 'axios'
+import thunk from 'redux-thunk'
+import Cookies from 'js-cookie'
+
+// Initial State
+const initState = {
+	jwt: '',
+	user: null,
+	loginError: ''
+}
+
+// Action Type
+const ON_LOGIN_SUCCESS = 'on_login_success'
+const ON_LOGIN_FAIL = 'on_login_fail'
+
+// function to return new state
+// (oldState, action) => newState
+export const reducer = (state = initState, action) => {
+	switch(action.type) {
+		case ON_LOGIN_SUCCESS: {
+			const newState = { ...state }
+			newState.jwt = action.payload.jwt
+			newState.user = action.payload.info
+			return newState
+		}
+		case ON_LOGIN_FAIL: {
+			const newState = { ...state }
+			newState.loginError = action.payload.error
+			return newState
+		}
+	}
+
+	return state
+}
+
+// Action Creator
+export const onLogin = (username, password, callback) => {
+	return (dispatch) => {
+		axios.post(`/api/login`, { 
+			username, 
+			password 
+		}).then(response => {
+			callback(response)
+			if (response.data.status) {
+				const data = {
+					jwt: response.data.jwt,
+					info: response.data.info
+				}
+				Cookies.set('jwt', data, { expires: 1, path: '/' })
+				return dispatch({
+					type: ON_LOGIN_SUCCESS,
+					payload: data
+				})
+			}
+		})
+	}
+}
+
+export const onJwtReceived = (userInfo) => {
+	return (dispatch) => {
+		return dispatch({
+			type: ON_LOGIN_SUCCESS,
+			payload: userInfo
+		})
+	}
+}
+
+export const onLogout = () => {
+	return (dispatch) => {
+		Cookies.remove('jwt', { path: '/' })
+		dispatch({
+			type: ON_LOGIN_SUCCESS,
+			payload: {
+				jwt: ''
+			}
+		})
+	}
+}
+
+export function initializeStore (initialState = initState) {
+  return createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunk))
+  )
+}
